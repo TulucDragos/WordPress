a6ac9547e065fc381de7360061a8fb91f0e62097
TulucDragos <33549864+TulucDragos@users.noreply.github.com>
2018-01-03 16:29:12 +0200

    Created the user code validation function ( it still needs some work ). resolved some CSS problems.

--- functions.php
+++ functions.php
@@ -4,46 +4,44 @@ function onpiste_styles()
 	if(is_page('On Piste Landing'))
 	{
 		wp_register_style('onpiste_style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
-    	wp_enqueue_style('onpiste_style'); // Enqueue it!
+    wp_enqueue_style('onpiste_style'); // Enqueue it!
 	}
 
 	if(is_page('Terms and Conditions') || is_page('Privacy Policy') )
 	{
 		wp_register_style('onpiste_terms_style', get_template_directory_uri() . '/css/termsandcond.css', array(), '1.0', 'all');
-    	wp_enqueue_style('onpiste_terms_style'); // Enqueue it!
+    wp_enqueue_style('onpiste_terms_style'); // Enqueue it!
 	}
- 
-    if(is_page('Log In') || is_page('Non-Partner') || is_page('Validate') || is_page('Thank You') || is_page('Not Validated') || is_page('Existing Code'))
-    {
+
+  if(is_page('Log In') || is_page('Non-Partner') || is_page('Validate') || is_page('Thank You') || is_page('Not Validated') || is_page('Existing Code') || is_page('Partner'))
+  {
     	wp_register_style('onpiste_login_style', get_template_directory_uri() . '/css/login.css', array(), '1.0', 'all');
     	wp_enqueue_style('onpiste_login_style'); // Enqueue it!
-    }
+  }
 
-     
+
 }
 
 function onpiste_partner_only()
 {
 	if(is_page('Partner') || is_page('Validate') || is_page('Thank You') || is_page('Not Validated') || is_page('Existing Code')) // insert in this if all other pages that should only be accessible to partners and administrators
 	{
-		//check if the user is logged in 
+		//check if the user is logged in
 		if(is_user_logged_in())
 		{
 			global $current_user;
 
-	    	$user_roles = $current_user->roles;
+	   	$user_roles = $current_user->roles;
 
-	    	$user_role = array_shift($user_roles);
-	    
-	    	$url = 'http://localhost/wordpress/non-partner/';
-   		
+	   	$user_role = array_shift($user_roles);
 
-			if( $user_role != 'partner' && $user_role != 'administrator')// check if the user role is not partner 
+	    $url = 'http://localhost/wordpress/non-partner/';
+
+			if( $user_role != 'partner' && $user_role != 'administrator')// check if the user role is not partner
 				{
-					wp_redirect( $url ); // redirect to the home page 
+					wp_redirect( $url ); // redirect to the home page
 					exit;
 				}
-			
 
 		}
 		else
@@ -51,87 +49,100 @@ function onpiste_partner_only()
 			wp_redirect('http://localhost/wordpress/login'); // if the user is not logged in than it redirects the user to the login page
 			exit;
 		}
-		
-	
+
 	}
 }
 
-
 function campaign_check ( $post_id)
 {
-	static $is_active = 0; 
-	
-	if('campaign' == get_post_type()) // do this only if the post type is campaign. 
+	static $is_active = 0;
+
+	if('campaign' == get_post_type()) // do this only if the post type is campaign.
 	{
 		if($_POST['action'] == 'editpost')
 			{
-					
-
 
 					$args = array (
 						'numberposts' => 50,
 						'post_type' => "campaign"
 					);
 
-
 					$all_posts = get_posts($args); // get all the campaign posts
-	
 
 					if(!empty($all_posts))
 					{
 							foreach($all_posts as $c_post)
 						{
 
-							if( get_post_status($c_post) == 'publish' && $_POST != $c_post) // count how many posts are active
-							{
-								$is_active++;
-								
-							}
+								if( get_post_status($c_post) == 'publish' && $_POST != $c_post) // count how many posts are active
+									{
+										$is_active++;
+									}
 						}
-					
+
 					}
 
-					if($is_active > 1 ) // check if there is anyother active 
+					if($is_active > 1 ) // check if there is anyother active
 					{
-						 $_POST['post_status'] = 'Draft'; // set the post as draft and display a message 
+						 $_POST['post_status'] = 'Draft'; // set the post as draft and display a message
 						 echo $_POST['post_status'];
-						 remove_action('save_post', 'campaign_check'); // un-hook the function because it is called when updating a post 
-						 wp_update_post($_POST); // update the post 
+						 remove_action('save_post', 'campaign_check'); // un-hook the function because it is called when updating a post
+						 wp_update_post($_POST); // update the post
 						 add_action('save_post', 'campaign_check'); // re-hook the function
-						 
+
 						 ?>
 		   				 <div class="error notice">
-		       				 <p><?php _e( 'There is already a campaign in progress', 'my_plugin_textdomain' ); ?></p> 
-		    			</div>
+		       				 <p><?php _e( 'There is already a campaign in progress', 'my_plugin_textdomain' ); ?></p>
+		    			 </div>
 		   				<?php
 		   				print_r($_POST);
-		   				
-
 					}
 
-					else 
+					else
 
 					{
 						?>
-						<div class="notice"> 
+						<div class="notice">
 		       				 <p><?php _e( 'The campaign has been activated', 'my_plugin_textdomain' ); ?></p>
 		    			</div>
 
 		   				<?php
-		   				
 
 					}
-		
-				
+
 			}
-		
+
 	}
 
 }
+function add_custom_query_var( $vars ){
+  $vars[] = "c";
+  return $vars;
+}
 
 
+function user_code_validation()
+{
+	if(is_page('Thank You'))
+	{
+			$my_c = get_query_var( 'c' );
+			if(is_page('Thank You') && $my_c != "")
+			{
+				print_r("variable was found");
+			}
+			else
+			{
+				print_r("not found");
+			}
+	}
+	
+	
+}
+
+add_filter( 'query_vars', 'add_custom_query_var' );
 add_action('save_post', 'campaign_check');
 add_action('wp', 'onpiste_partner_only');
+add_action('wp', 'user_code_validation');
 add_action('wp_enqueue_scripts', 'onpiste_styles');
 
 ?>
--- validate.php
+++ validate.php
@@ -6,44 +6,66 @@
 	<div class="top_content">
 		<div class="header">
 			<div class="logo_content">
-	
+
 			</div>
-			
-			
+<?php 
+$my_c = get_query_var( 'c' );
+?>
+
 		</div>
 	</div>
 
 	<div class="shadow">
-		
+
 	</div>
 	<div class="white_content">
 		<div class="top_content">
 			<div class="header">
 				<div class="coming_soon">
-					
+
 				</div>
-				
-				
-			
+
+
+				<h3>Active Campaign:
+
+				<?php
+
+				$the_query = new WP_Query(array(
+        				'post_type' => 'campaign'
+
+    					) );
+
+				 if ( $the_query->have_posts() ) : ?>
+					<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+					 <?php the_title(); ?> </h3>
+
+					<?php endwhile;
+					wp_reset_postdata();
+					?>
+				<?php else : ?>
+					<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+				<?php endif; ?>
 
 				<div class = "terms">
+
 
 					<?php the_content(); ?>
 
 					<div class="val">
-						<button id= "valideaza">Valideaza</button>
+						<button id= "valideaza"><a href="<?php echo esc_url( add_query_arg( 'c', $my_c, site_url( '/thank-you/' ) ) )?>"/>Valideaza</button>
 					</div>
 					<div class="not-val">
-						<button id= "nu-valideaza">Nu acum</button>
+						<button id= "nu-valideaza"><a href="#"/>Nu acum</button>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	
+
 </div>
 
-<?php 
+<?php
 	endwhile;
 	endif;
 ?>
